d05c0f64d31ea8de8630da7e4fed58790af36efa
ddamjanovic <damjanovic27@gmail.com>
2022-07-22 12:06:22 +0200

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { Provider } from 'react-redux';
+import type { AppProps } from 'next/app';
+import { store } from '../redux/store';
+
+const MyApp = ({
+  Component, pageProps,
+}: AppProps) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp
